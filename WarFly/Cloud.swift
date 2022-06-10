0d95e728d7cab3620e0af400d76059632b8e04fe
mrDuderino <36927431+mrDuderino@users.noreply.github.com>
2022-06-10 18:59:31 +0300

    islands + clouds + plane

--- WarFly/Cloud.swift
+++ WarFly/Cloud.swift
@@ -9,17 +9,31 @@ import SpriteKit
 import GameplayKit
 
 protocol GameBackgroundSpriteable {
-    static func populateSprite(at point: CGPoint) -> Self
+    static func populate() -> Self
+    static func randomPoint() -> CGPoint
+}
+
+extension GameBackgroundSpriteable {
+    static func randomPoint() -> CGPoint {
+        let screen = UIScreen.main.bounds
+        let distribution = GKRandomDistribution(lowestValue: Int(screen.size.height) + 100, highestValue: Int(screen.size.height) + 200)
+        let y = CGFloat(distribution.nextInt())
+        let x = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: Int(screen.size.width)))
+        
+        return CGPoint(x: x, y: y)
+    }
 }
 
 final class Cloud: SKSpriteNode, GameBackgroundSpriteable {
 
-    static func populateSprite(at point: CGPoint) -> Cloud {
+    static func populate() -> Cloud {
         let cloudImageName = configureName()
         let cloud = Cloud(imageNamed: cloudImageName)
         cloud.setScale(randomScaleFactor)
-        cloud.position = point
+        cloud.position = randomPoint()
         cloud.zPosition = 10
+        cloud.run(move(from: cloud.position))
+        
         return cloud
     }
     
@@ -27,13 +41,23 @@ final class Cloud: SKSpriteNode, GameBackgroundSpriteable {
         let distribution = GKRandomDistribution(lowestValue: 1, highestValue: 3)
         let randomNumber = distribution.nextInt()
         let imageName = "cl\(randomNumber)"
+        
         return imageName
     }
     
     private static var randomScaleFactor: CGFloat {
         let distribution = GKRandomDistribution(lowestValue: 20, highestValue: 30)
         let randomNumber = CGFloat(distribution.nextInt()) / 10
+        
         return randomNumber
     }
 
+    private static func move(from point: CGPoint) -> SKAction {
+        let movePoint = CGPoint(x: point.x, y: -200)
+        let moveDistance = point.y + 200
+        let movementSpeed: CGFloat = 150.0
+        let duration = moveDistance / movementSpeed
+        
+        return SKAction.move(to: movePoint, duration: duration)
+    }
 }
--- WarFly/GameScene.swift
+++ WarFly/GameScene.swift
@@ -7,28 +7,63 @@
 
 import SpriteKit
 import GameplayKit
+import CoreMotion
 
 class GameScene: SKScene {
     
+    var player: SKSpriteNode!
+    let motionManager = CMMotionManager()
+    var xAcceleration: CGFloat = 0
+    
     override func didMove(to view: SKView) {
         
+        configureStartScene()
+    }
+    
+    private func configureStartScene() {
         let screenCenterPoint = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
         let background = Background.populateBackground(at: screenCenterPoint)
         background.size = self.size
         self.addChild(background)
         
         let screen = UIScreen.main.bounds
-        for _ in 1...5 {
-            let x: CGFloat = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: Int(screen.size.width)))
-            let y: CGFloat = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: Int(screen.size.height)))
-            
-            let island = Island.populateSprite(at: CGPoint(x: x, y: y))
-            self.addChild(island)
-            
-            let cloud = Cloud.populateSprite(at: CGPoint(x: x, y: y))
-            self.addChild(cloud)
-        }
         
+//        let x: CGFloat = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: Int(screen.size.width)))
+//        let y: CGFloat = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: Int(screen.size.height)))
+//
+        let island1 = Island.populate(at: CGPoint(x: 100, y: 200))
+        self.addChild(island1)
+        
+        let island2 = Island.populate(at: CGPoint(x: screen.size.width - 100, y: screen.size.height - 200))
+        self.addChild(island2)
+        
+//        let cloud = Cloud.populate()
+//        self.addChild(cloud)
+        
+        player = PlayerPlane.populate(at: CGPoint(x: screen.size.width / 2, y: 100))
+        self.addChild(player)
+        
+        motionManager.accelerometerUpdateInterval = 0.2
+        motionManager.startAccelerometerUpdates(to: OperationQueue.current!) { data, error in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            if let data = data {
+                let acceleration = data.acceleration
+                self.xAcceleration = CGFloat(acceleration.x) * 0.7 + self.xAcceleration * 0.3
+            }
+        }
+    }
+    
+    override func didSimulatePhysics() {
+        super.didSimulatePhysics()
+        
+        player.position.x += xAcceleration * 50
+        if player.position.x < -70 {
+            player.position.x = self.size.width + 70
+        } else if player.position.x > self.size.width + 70 {
+            player.position.x = -70
+        }
     }
     
 }
